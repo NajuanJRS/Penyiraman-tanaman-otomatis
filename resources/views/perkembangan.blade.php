71d6058c3434e306a41556affab0244a89aabfe6
NajuanJRS <najuan600@gmail.com>
2026-05-22 20:51:52 +0800

    Perbaikan preview gambar

--- resources/views/perkembangan.blade.php
+++ resources/views/perkembangan.blade.php
@@ -65,19 +65,13 @@ Swal.fire({
 <div class="modal fade" id="imageModal" tabindex="-1">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
-
             <div class="modal-header">
                 <h5 class="modal-title">Preview Gambar</h5>
-
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
-
             <div class="modal-body text-center">
-
-                <img id="modalImage" alt="Gambar Tanaman" src="" class="img-fluid rounded" style="max-height: 80vh;">
-
+                <img id="modalImage" alt="Gambar Tanaman" src="" class="preview-modal-img">
             </div>
-
         </div>
     </div>
 </div>
