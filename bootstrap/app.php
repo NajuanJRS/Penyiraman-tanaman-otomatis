b9bb562e4f59b4534a9b9fb4618a958434d0d73f
NajuanJRS <najuan600@gmail.com>
2026-06-16 18:01:40 +0800

    Perbaikan error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Percayai semua proxy (Nginx reverse proxy di VPS)
-        // agar asset() menghasilkan URL https:// yang benar
-        $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'checklogin' => \App\Http\Middleware\CheckLogin::class,
